5bcf9cede10f0c8fc3cd34faa6afcb2888255bc7
Florian Konnertz <f.konnertz@gmail.com>
2013-09-10 11:01:39 +0200

    minor

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # users
-drush user-import profiles/workflow/data/userimport.csv
+drush dl user_import
+drush en user_import
+drush user-import ../../profiles/workflow/data/userimport.csv
 
 # site related
+# language
+drush dl drush_language
+drush en drush_language
 drush language-add de
 drush language-default de
+
+
